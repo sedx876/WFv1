6e144b8240f400b5f646cb21e86839180dfa0862
sedx876 <swatkins876@gmail.com>
2021-02-12 06:47:40 -0500

    file cleanup

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './index.css'
+import { Typography } from '@material-ui/core'
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1><strong className='green-text text-darken-4 title'>WeedFeed</strong></h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
