1b1dd76f62b0c8cc27b7576e8a471c9e117da3e0
sedx876 <swatkins876@gmail.com>
2021-02-16 07:56:31 -0500

    Added isPoster and updatePost in post controller

--- API/controllers/post.js
+++ API/controllers/post.js
@@ -88,3 +88,17 @@ exports.postsByUser = (req, res) => {
       res.json(posts)
     })
 }
+
+exports.isPoster = (req, res, next) => {
+  let sameUser = req.post && req.auth && req.post.postedBy._id == req.auth._id 
+  let adminUser = req.post && req.auth && req.auth.role === 'admin'
+  console.table("req.post ", req.post, " req.auth ", req.auth)
+  console.table("SAMEUSER: ", sameUser, " ADMINUSER: ", adminUser)
+  let isPoster = sameUser || adminUser
+  if (!isPoster){
+    return res.status(403).json({
+      error: 'User Is Not Authorized'
+    })
+  }
+  next()
+}
