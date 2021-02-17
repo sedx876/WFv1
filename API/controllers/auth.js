9ad6e4b85ffd4a27b6d573f40c33481f605ba35e
sedx876 <swatkins876@gmail.com>
2021-02-17 10:08:50 -0500

    work on auth controller

--- API/controllers/auth.js
+++ API/controllers/auth.js
@@ -94,3 +94,79 @@ exports.forgotPassword = (req, res) => {
     })
   })
 }
+
+// to allow user to reset password
+// first you will find the user in the database with user's resetPasswordLink
+// user model's resetPasswordLink's value must match the token
+// if the user's resetPasswordLink(token) matches the incoming req.body.resetPasswordLink(token)
+// then we got the right user
+exports.resetPassword = (req, res) =>{
+  const { resetPasswordLink, newPassword } = req.body
+  User.findOne({ resetPasswordLink}, (err, user) =>{
+    //If err or no user
+    if (err || !user)
+    return res.status('401').json({
+      error: 'Invalid Link!'
+    })
+    const updatedFields = {
+      password: newPassword,
+      resetPasswordLink: ''
+    }
+    user = _.extend(user, updatedFields)
+    user.updated = Date.now()
+    user.save((err, result) => {
+      if (err){
+        return res.status(400).json({
+          error: err
+        })
+      }
+      res.json({
+        message: `Great! Now You Can LogIn with Your New Password.`
+      })
+    })
+  })
+}
+
+//const client new OAuth2Client(process.env.REACT_APP_GOOGLE_CLIENT_ID)
+
+exports.socialLogin = async (req, res) => {
+  const idToken = req.body.tokenId
+  const ticket = await client.verifyIdToken({
+    idToken, audience: process.env.REACT_APP_GOOGLE_CLIENT_ID
+  })
+  console.table('ticket', ticket)
+  const { email_verified, email, name, picture, sub: googleid } = ticket.getPayload()
+  if (email_verified){
+    console.table(`email_verified > ${email_verified}`)
+    const newUser = { email, name, password: googleid }
+    //Try Signup by finding user with req.email
+    let user = User.findOne({ email }, (err, user) => {
+      if (err || !user){
+        //Create a New User and login
+        user = new User(newUser)
+        req.profile = user
+        user.save()
+        //Generate a token with user id and secret
+        const token = jwt.sign({ _id: user._id, iss: process.env.APP_NAME}, process.env.JWT_SECRET)
+        res.cookie('t', token, { expire: new Date() + 9999 })
+        //Return response with user and token to frontend
+        const { _id, name, email } = user 
+        return res.json({ token, user: { _id, name, email } })
+      }else{
+        //Update existing user with new social info and login
+        req.profile = user 
+        user = _.extend(user, newUser)
+        user.updated = Date.now()
+        user.save()
+        //Generate a token with user id and secret
+        const token = jwt.sign({ _id: user._id, iss: process.env.APP_NAME },
+          process.env.JWT_SECRET
+        )
+        res.cookie('t', token, { expire: new Date() + 9999 })
+        //Return response with user nad token to frontend
+        const { _id, name, email } = user 
+        return res.json({ token, user: { _id, name, email }})
+      }
+    })
+  }
+}
