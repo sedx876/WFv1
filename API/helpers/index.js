6f6305475fff1b0ad395aa7f7b76ccdcbcbe5c69
sedx876 <swatkins876@gmail.com>
2021-02-18 07:49:57 -0500

    Added helpers/index.js

---
+++ API/helpers/index.js
@@ -0,0 +1,20 @@
+const nodeMailer = require("nodemailer")
+
+const defaultEmailData = { from: "noreply@node-react.com" }
+
+exports.sendEmail = emailData => {
+  const transporter = nodeMailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    requireTLS: true,
+    auth: {
+      user: "swatkins876@gmail.com",
+      pass: "quxrnqrjhfqdvlgr"
+    }
+  })
+  return transporter
+    .sendMail(emailData)
+    .then(info => console.log(`Message sent: ${info.response}`.green.inverse))
+    .catch(err => console.log(`Problem sending email: ${err}`.red.inverse))
+}
