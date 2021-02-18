31d53863d3b7ecc99db1687cd0023c6f03a46721
sedx876 <swatkins876@gmail.com>
2021-02-18 13:06:56 -0500

    Added user/apiUser.js

--- client/src/MainRouter.js
+++ client/src/MainRouter.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
-import Navbar from './components/Navbar'
-import Home from './components/Home'
-import Footer from './components/Footer'
+import Navbar from './core/Navbar'
+import Home from './core/Home'
+import Footer from './core/Footer'
 
 const MainRouter = () => {
   return (
