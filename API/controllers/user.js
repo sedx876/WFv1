7164aa44516108d15ea8a7cad22f5e91db81dded
sedx876 <swatkins876@gmail.com>
2021-02-18 07:30:41 -0500

    work on user controller

--- API/controllers/user.js
+++ API/controllers/user.js
@@ -18,3 +18,72 @@ exports.userById = (req, res, next, id) => {
     next()
   })
 }
+
+exports.hasAuthorization = (req, res, next) => {
+  let sameUser = req.profile && req.auth && req.profile._id == req.auth._id 
+  let adminUser = req.profile && req.auth && req.auth.role === 'admin'
+  const authorized = sameUser || adminUser 
+  console.table('req.profile', req.profile, 'req.auth', req.auth)
+  console.table('SAMEUSER', sameUser, 'ADMINUSER', adminUser)
+  if (!authorized){
+    return res.status(403).json({
+      error: 'User Is Not Authorized To Perform This Action'
+    })
+  }
+  next()
+}
+
+exports.allUsers = (req, res) => {
+  User.find((err, users) => {
+    if (err){
+      return res.status(400).json({
+        error: err
+      })
+    }
+    res.json(users)
+  }).select('name email updated created role')
+}
+
+exports.getUser = (req, res) => {
+  req.profile.hashed_password = undefined
+  req.profile.salt = undefined 
+  return res.json(req.profile)
+}
+
+exports.updateUser = (req, res, next) => {
+  let form = new formidable.IncomingForm()
+  console.table('Imcoming Form Data: ', user)
+  form.keepExtensions = true
+  form.parse(req, (err, fields, files) => {
+    if (err){
+      return res.status(400).json({
+        error: 'Photo Could Not Be Uploaded'
+      })
+    }
+    //Save User
+    let user = req.profile
+    console.table('User In Update: ', user)
+    user = _.extend(user, fields)
+    user.updated = Date.now()
+    console.table('User Form Data Update: ', user)
+    if (files.photo){
+      user.photo.data = fs.readFileSync(files.photo.path)
+      user.photo.contentType = files.photo.type
+    }
+    user.save((err, result) => {
+      if (err){
+        return res.status(400).json({
+          error: err
+        })
+      }
+      user.hashed_password = undefined 
+      user.salt = undefined
+      console.table('User After Update with formData: ', user)
+      res.json(user)
+    })
+  })
+}
+
+exports.userPhoto = (req, res) => {
+  
+}
