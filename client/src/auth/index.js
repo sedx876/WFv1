515171d0d18193559009591f218ee748ac07e3cc
sedx876 <swatkins876@gmail.com>
2021-02-19 08:25:51 -0500

    work on auth/index.js

--- client/src/auth/index.js
+++ client/src/auth/index.js
@@ -35,13 +35,84 @@ export const authenticate = (jwt, next) => {
 	}
 }
 
+export const setName = (name, next) => {
+	if (typeof window !== 'undefined') {
+		localStorage.setItem('username', JSON.stringify(name))
+		next()
+	}
+}
+
+export const signout = next => {
+	if (typeof window !== 'undefined') localStorage.removeItem('jwt')
+	next()
+	return fetch(`${process.env.REACT_APP_API_URL}/signout`, {
+		method: 'GET'
+	})
+		.then(response => {
+			console.log('signout', response)
+			return response.json()
+		})
+			.catch(err => console.log(err))
+}
+
 export const isAuthenticated = () => {
 	if (typeof window == 'undefined') {
 			return false
 	}
+
 	if (localStorage.getItem('jwt')) {
 		return JSON.parse(localStorage.getItem('jwt'))
 	} else {
 		return false
 	}
+}
+
+export const forgotPassword = email => {
+	console.table('email: ', email)
+	return fetch(`${process.env.REACT_APP_API_URL}/forgot-password/`, {
+		method: 'PUT',
+		headers: {
+			Accept: 'application/json',
+			'Content-Type': 'application/json'
+		},
+			body: JSON.stringify({ email })
+	})
+			.then(response => {
+				console.table('forgot password response: ', response)
+				return response.json()
+			})
+			.catch(err => console.log(err))
+}
+
+export const resetPassword = resetInfo => {
+	return fetch(`${process.env.REACT_APP_API_URL}/reset-password/`, {
+		method: 'PUT',
+		headers: {
+			Accept: 'application/json',
+			'Content-Type': 'application/json'
+		},
+			body: JSON.stringify(resetInfo)
+	})
+			.then(response => {
+				console.log('forgot password response: ', response)
+				return response.json()
+		})
+			.catch(err => console.log(err))
+}
+
+export const socialLogin = user => {
+	return fetch(`${process.env.REACT_APP_API_URL}/social-login/`, {
+		method: 'POST',
+		headers: {
+			Accept: 'application/json',
+			'Content-Type': 'application/json'
+		},
+			// credentials: "include", // works only in the same origin
+			body: JSON.stringify(user)
+	})
+			.then(response => {
+					console.log('signin response: ', response)
+					return response.json()
+			})
+			.catch(err => console.log(err))
 }
