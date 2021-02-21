54e8400241de9860a4d20e3191c760aaed9ad43d
sedx876 <swatkins876@gmail.com>
2021-02-21 08:30:56 -0500

    Added NewPost.js

---
+++ client/src/post/NewPost.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import { isAuthenticated } from '../auth'
+import { create } from './apiPost'
+import { Redirect } from 'react-router-dom'
+
+class NewPost extends Component {
+  constructor(){
+    super()
+    this.state = {
+      title: '',
+      body: '',
+      photo: '',
+      error: '',
+      user: {},
+      fileSize: 0,
+      loading: false,
+      redirectToProfile: false
+    }
+  }
+
+  
+
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default NewPost;
